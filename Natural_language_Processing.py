79a7de12a5c811eab1629952e7956b1ff91d51e4
Rajnish <38453583+Rajnish20@users.noreply.github.com>
2019-06-07 16:26:41 +0530

    Natural Language Processing Done

--- Natural_language_Processing.py
+++ Natural_language_Processing.py
@@ -10,5 +10,42 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
 #cleaning the text
 import re
-review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
-review = review.lower()
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords 
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    #using set in case if we have a bigger sentence or a book that we have to remove irrelevant text from
+    review = [word for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review) # Back to the original review but clean
+    corpus.append(review)
+    
+# Creating the Bag of words through Tokenization
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+#splitting the dataset into train and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+#Fitting Naive_bayes to the Training Set
+from sklearn.naive_bayes import GaussianNB 
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+
+#Predecting the results of Test Set
+y_pred = classifier.predict(X_test)
+
+#Creating the Confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
